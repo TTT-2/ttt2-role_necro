19def50ef430c26016c3e2303859ee8450fb12e1
MisterClems <73431903+MisterClems@users.noreply.github.com>
2021-05-28 12:03:00 +0200

    french update  (#15)

--- lua/terrortown/lang/fr/necromancer.lua
+++ lua/terrortown/lang/fr/necromancer.lua
@@ -7,8 +7,8 @@ L["hilite_win_" .. NECROMANCER.defaultTeam] = "LE NÉCROMANCIEN A GAGNÉ"
 L["win_" .. NECROMANCER.defaultTeam] = "Le Nécromancien a gagné!"
 L["info_popup_" .. NECROMANCER.name] = [[Maintenant, c'est à votre tour ! Essayez de faire combattre des joueurs morts à vos côtés!]]
 L["body_found_" .. NECROMANCER.abbr] = "C'était un Nécromancien!"
-L["search_role_" .. NECROMANCER.abbr] = "Cette personne était un Nécromancien!"
-L["ev_win_" .. NECROMANCER.defaultTeam] = "Le méchant Nécromancien a gagné la manche!"
+L["search_role_" .. NECROMANCER.abbr] = "C'était un Nécromancien!"
+L["ev_win_" .. NECROMANCER.defaultTeam] = "Le méchant Nécromancien a gagné le round!"
 L["target_" .. NECROMANCER.name] = "Nécromancien"
 L["ttt2_desc_" .. NECROMANCER.name] = [[Le Nécromancien doit faire revivre les joueurs morts. Si un joueur est réanimé, il ressemble à un zombie et mourra si ses munitions sont vides.]]
 
@@ -16,11 +16,11 @@ L[ZOMBIE.name] = "Zombie"
 L["target_" .. ZOMBIE.name] = "Zombie"
 L["ttt2_desc_" .. ZOMBIE.name] = [[Vous devez gagner avec votre compagnon!]]
 L["body_found_" .. ZOMBIE.abbr] = "C'était un Zombie!"
-L["search_role_" .. ZOMBIE.abbr] = "Cette personne était un Zombie!"
+L["search_role_" .. ZOMBIE.abbr] = "C'était un Zombie!"
 
 -- OTHER ROLE LANGUAGE STRINGS
-L["necro_defi_name"] = "Le défi du Nécromancien"
-L["zombie_pistol_name"] = "Deagle de Zombie"
+L["necro_defi_name"] = "Défibrillateur du Nécromancien"
+L["zombie_pistol_name"] = "Deagle du Zombie"
 
 L["revived_by_necromancer"] = "Vous êtes réanimé par {name} vous devenez un Zombie du Nécromancien. Préparez-vous!"
 L["necrodefi_hold_key_to_revive"] = "Maintenez [{key}] pour réanimer le joueur en tant que Zombie"
