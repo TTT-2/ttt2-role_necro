fe453610e3cc27a7de110180d9f8e17866b15d25
Tim Goll <github@timgoll.de>
2024-02-04 23:50:10 +0100

    using new markerVision API

--- lua/terrortown/entities/roles/necromancer/shared.lua
+++ lua/terrortown/entities/roles/necromancer/shared.lua
@@ -56,13 +56,16 @@ if SERVER then
 	hook.Add("TTTOnCorpseCreated", "NecroAddedDeadBody", function(rag, ply)
 		if not IsValid(rag) or not IsValid(ply) then return end
 
-		markerVision.RegisterEntity(rag, ROLE_NECROMANCER, VISIBLE_FOR_ROLE)
+		local mvObject = rag:AddMarkerVision("corpse_necro")
+		mvObject:SetOwner(ROLE_NECROMANCER)
+		mvObject:SetVisibleFor(VISIBLE_FOR_ROLE)
+		mvObject:SyncToClients()
 	end)
 
 	hook.Add("EntityRemoved", "NecroRemovedDeadBody", function(ent)
 		if not IsValid(ent) or ent:GetClass() ~= "prop_ragdoll" then return end
 
-		markerVision.RemoveEntity(ent)
+		ent:RemoveMarkerVision("corpse_necro")
 	end)
 
 	-- modify roles table of rolesetup addon
@@ -134,8 +137,9 @@ if CLIENT then
 	hook.Add("TTT2RenderMarkerVisionInfo", "HUDDrawMarkerVisionNecroCorpse", function(mvData)
 		local client = LocalPlayer()
 		local ent = mvData:GetEntity()
+		local mvObject = mvData:GetMarkerVisionObject()
 
-		if not client:IsTerror() or not IsValid(ent) or ent:GetClass() ~= "prop_ragdoll" then return end
+		if not client:IsTerror() or not mvObject:IsObjectFor(ent, "corpse_necro") then return end
 
 		local distance = math.Round(util.HammerUnitsToMeters(mvData:GetEntityDistance()), 1)
 
@@ -145,6 +149,6 @@ if CLIENT then
 		mvData:SetTitle(ParT("necro_corpse_player", {nick = CORPSE.GetPlayerNick(ent, "---")}))
 
 		mvData:AddDescriptionLine(ParT("marker_vision_distance", {distance = distance}))
-		mvData:AddDescriptionLine(TryT("marker_vision_visible_for_" .. markerVision.GetVisibleFor(ent)), COLOR_SLATEGRAY)
+		mvData:AddDescriptionLine(TryT(mvObject:GetVisibleForTranslationKey()), COLOR_SLATEGRAY)
 	end)
 end
