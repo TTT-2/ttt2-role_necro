1bd20d507331443ee63d74a194256d6d87c8128a
Tim Goll <github@timgoll.de>
2024-08-18 00:52:49 +0200

    refactored defi to use inherit

--- gamemodes/terrortown/entities/weapons/weapon_ttth_necrodefi.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttth_necrodefi.lua
@@ -1,393 +1,43 @@
 if SERVER then
-	AddCSLuaFile()
-
-	resource.AddFile("materials/gui/ttt/icon_defi_necro.vmt")
+    AddCSLuaFile()
 end
 
-local DEFI_IDLE = 0
-local DEFI_BUSY = 1
-local DEFI_CHARGE = 2
+local flags = { FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED }
 
-local DEFI_ERROR_NO_SPACE = 1
-local DEFI_ERROR_TOO_FAST = 2
-local DEFI_ERROR_LOST_TARGET = 3
-local DEFI_ERROR_NO_VALID_PLY = 4
-local DEFI_ERROR_ALREADY_REVIVING = 5
-local DEFI_ERROR_FAILED = 6
-local DEFI_ERROR_ZOMBIE = 7
-local DEFI_ERROR_PLAYER_ALIVE = 8
-local DEFI_ERROR_PLAYER_DISCONNECTED = 9
-
-SWEP.Base = "weapon_tttbase"
+SWEP.Base = "weapon_ttt_defibrillator"
 
 if CLIENT then
-	SWEP.ViewModelFOV = 78
-	SWEP.DrawCrosshair = false
-	SWEP.ViewModelFlip = false
+    SWEP.EquipMenuData = {
+        type = "item_weapon",
+        name = "necro_defi_name",
+        desc = "necro_defi_desc",
+    }
 
-	SWEP.EquipMenuData = {
-		type = "item_weapon",
-		name = "necro_defi_name",
-		desc = "necro_defi_desc"
-	}
-
-	SWEP.Icon = "vgui/ttt/icon_defi_necro"
+    SWEP.Icon = "vgui/ttt/icon_defi_necro"
 end
 
 SWEP.Kind = WEAPON_EQUIP2
 SWEP.CanBuy = nil
 SWEP.notBuyable = true
 
-SWEP.UseHands = true
-SWEP.ViewModel = "models/weapons/v_c4.mdl"
-SWEP.WorldModel = "models/weapons/w_c4.mdl"
-
-SWEP.AutoSpawnable = false
-SWEP.NoSights = true
-
-SWEP.HoldType = "pistol"
-SWEP.LimitedStock = true
-
-SWEP.Primary.Recoil = 0
-SWEP.Primary.ClipSize = 1
-SWEP.Primary.DefaultClip = 1
-SWEP.Primary.Automatic = false
-SWEP.Primary.Delay = 1
-SWEP.Primary.Ammo = "none"
-
-SWEP.Secondary.Recoil = 0
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = -1
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Delay = 0.5
-
-SWEP.Charge = 0
-SWEP.Timer = -1
+SWEP.EnableConfigurableClip = true
+SWEP.ConfigurableClip = 3
 
 SWEP.AllowDrop = false
 
+SWEP.cvars = {
+    reviveBraindead = CreateConVar("ttt_defibrillator_revive_braindead", "1", flags),
+    playSound = CreateConVar("ttt_defibrillator_play_sounds", "0", flags),
+    reviveTime = CreateConVar("ttt_defibrillator_revive_time", "3.0", flags),
+    errorTime = CreateConVar("ttt_defibrillator_error_time", "1.5", flags),
+    successChance = CreateConVar("ttt_defibrillator_success_chance", "100", flags),
+    resetConfirmation = CreateConVar("ttt_defibrillator_reset_confirm", "0", flags),
+}
+
+SWEP.revivalReason = "revived_by_necromancer"
+
 if SERVER then
-	function SWEP:OnDrop()
-		self.BaseClass.OnDrop(self)
-
-		self:CancelRevival()
-		self:Remove()
-	end
-
-	function SWEP:SetState(state)
-		self:SetNWInt("defi_state", state or DEFI_IDLE)
-	end
-
-	function SWEP:Reset()
-		self:SetState(DEFI_IDLE)
-
-		self.defiTarget = nil
-		self.defiBone = nil
-		self.defiStart = 0
-
-		self.defiTimer = nil
-	end
-
-	function SWEP:Error(type, errorEnt)
-		self:SetState(DEFI_CHARGE)
-
-		self.defiTarget = nil
-		self.defiTimer = "defi_reset_timer_" .. self:EntIndex()
-
-		if timer.Exists(self.defiTimer) then return end
-
-		timer.Create(self.defiTimer, GetConVar("ttt2_necrodefi_error_time"):GetFloat(), 1, function()
-			if not IsValid(self) then return end
-
-			self:Reset()
-		end)
-
-		-- In case people want to do something about this for themselves, presuambly they want to suppress the Message call.
-		local defibErrorResult = hook.Run("TTT2NecroDefibError", type, self, self:GetOwner(), errorEnt)
-		if defibErrorResult ~= nil then return end
-
-		self:Message(type)
-	end
-
-	function SWEP:Message(type)
-		local owner = self:GetOwner()
-
-		if type == DEFI_ERROR_NO_SPACE then
-			LANG.Msg(owner, "necrodefi_error_no_space", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_TOO_FAST then
-			LANG.Msg(owner, "necrodefi_error_too_fast", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_LOST_TARGET then
-			LANG.Msg(owner, "necrodefi_error_lost_target", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_NO_VALID_PLY then
-			LANG.Msg(owner, "necrodefi_error_no_valid_ply", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_ALREADY_REVIVING then
-			LANG.Msg(owner, "necrodefi_error_already_reviving", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_FAILED then
-			LANG.Msg(owner, "necrodefi_error_failed", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_ZOMBIE then
-			LANG.Msg(owner, "necrodefi_error_zombie", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_PLAYER_ALIVE then
-			LANG.Msg(owner, "necrodefi_error_player_alive", nil, MSG_MSTACK_WARN)
-		elseif type == DEFI_ERROR_PLAYER_DISCONNECTED then
-			LANG.Msg(owner, "necrodefi_error_player_disconnected", nil, MSG_MSTACK_WARN)
-		elseif isstring(type) then
-			LANG.Msg(owner, type, nil, MSG_MSTACK_WARN)
-		end
-	end
-
-	function SWEP:BeginRevival(ragdoll, bone)
-		local ply = CORPSE.GetPlayer(ragdoll)
-		local owner = self:GetOwner()
-
-		if not IsValid(ply) then
-			self:Error(DEFI_ERROR_NO_VALID_PLY, ragdoll)
-
-			return
-		end
-
-		if ply:IsReviving() then
-			self:Error(DEFI_ERROR_ALREADY_REVIVING, ragdoll)
-
-			return
-		end
-
-		if ply:IsActive() then
-			self:Error(DEFI_ERROR_PLAYER_ALIVE, ragdoll)
-
-			return
-		end
-
-		local reviveTime = GetConVar("ttt2_necrodefi_revive_time"):GetFloat()
-
-		self:SetState(DEFI_BUSY)
-		self:SetStartTime(CurTime())
-		self:SetReviveTime(reviveTime)
-
-		-- start revival
-		ply:Revive(
-			reviveTime,
-			function(p)
-				AddZombie(p, owner)
-			end,
-			function(p)
-				return not p:IsActive()
-			end,
-			true,
-			REVIVAL_BLOCK_ALL
-		)
-		ply:SendRevivalReason("revived_by_necromancer", {name = self:GetOwner():Nick()})
-
-		self.defiTarget = ragdoll
-		self.defiBone = bone
-	end
-
-	function SWEP:FinishRevival()
-		self:Reset()
-
-		self:SetClip1(self:Clip1() - 1)
-
-		if self:Clip1() < 1 then
-			self:Remove()
-
-			RunConsoleCommand("lastinv")
-		end
-	end
-
-	function SWEP:CancelRevival()
-		local ply = CORPSE.GetPlayer(self.defiTarget)
-
-		self:Reset()
-
-		if not IsValid(ply) then return end
-
-		ply:CancelRevival()
-		ply:SendRevivalReason(nil)
-	end
-
-	function SWEP:SetStartTime(time)
-		self:SetNWFloat("defi_start_time", time or 0)
-	end
-
-	function SWEP:SetReviveTime(time)
-		self:SetNWFloat("defi_revive_time", time or 0)
-	end
-
-	function SWEP:Think()
-		if self:GetState() ~= DEFI_BUSY then return end
-
-		local owner = self:GetOwner()
-		local target = CORPSE.GetPlayer(self.defiTarget)
-
-		if CurTime() >= self:GetStartTime() + GetConVar("ttt2_necrodefi_revive_time"):GetFloat() - 0.01 then
-			self:FinishRevival()
-		elseif not owner:KeyDown(IN_ATTACK) or owner:GetEyeTrace(MASK_SHOT_HULL).Entity ~= self.defiTarget then
-			self:CancelRevival()
-			self:Error(DEFI_ERROR_LOST_TARGET, self.defiTarget)
-		elseif target:IsActive() then
-			self:CancelRevival()
-			self:Error(DEFI_ERROR_PLAYER_ALIVE, target)
-		end
-	end
-
-	function SWEP:Initialize()
-		local ammo = 1
-		local playerAlive = 0
-
-		local plys = player.GetAll()
-
-		for i = 1, #plys do
-			local ply = plys[i]
-
-			if not ply:GetForceSpec() then
-				playerAlive = playerAlive + 1
-			end
-		end
-
-		ammo = ammo + playerAlive * GetGlobalFloat("ttt2_necrodefi_ammo", 0.34)
-
-		if ammo > 1 then
-			ammo = ammo - 1
-		end
-
-		self:SetClip1(ammo)
-	end
-
-	function SWEP:PrimaryAttack()
-		local owner = self:GetOwner()
-
-		local trace = owner:GetEyeTrace(MASK_SHOT_HULL)
-		local distance = trace.StartPos:Distance(trace.HitPos)
-		local ent = trace.Entity
-
-		if distance > 100 or not IsValid(ent)
-			or ent:GetClass() ~= "prop_ragdoll"
-			or not CORPSE.IsValidBody(ent)
-		then return end
-
-		local defibAttemptResult = hook.Run("TTT2AttemptNecroDefibPlayer", owner, ent, self)
-		if defibAttemptResult ~= nil then
-			self:Error(nil, ent)
-			return
-		end
-
-		local corpsePlayer = CORPSE.GetPlayer(ent)
-		if not IsValid(corpsePlayer) then
-			self:Error(DEFI_ERROR_PLAYER_DISCONNECTED, ent)
-
-			return
-		end
-
-		if self:GetState() ~= DEFI_IDLE then
-			self:Error(DEFI_ERROR_TOO_FAST, ent)
-
-			return
-		end
-
-		if CORPSE.GetPlayer(ent):GetSubRole() == ROLE_ZOMBIE then
-			self:Error(DEFI_ERROR_ZOMBIE, ent)
-
-			return
-		end
-
-		local spawnPoint = plyspawn.MakeSpawnPointSafe(corpsePlayer, ent:GetPos())
-		if not spawnPoint then
-			self:Error(DEFI_ERROR_NO_SPACE, ent)
-		else
-			self:BeginRevival(ent, trace.PhysicsBone)
-		end
-	end
-end
-
--- do not play sound when swep is empty
-function SWEP:DryFire()
-	return false
-end
-
-function SWEP:GetState()
-	return self:GetNWInt("defi_state", DEFI_IDLE)
-end
-
-function SWEP:GetStartTime()
-	return self:GetNWFloat("defi_start_time", 0)
-end
-
-function SWEP:GetReviveTime()
-	return self:GetNWFloat("defi_revive_time", 0)
-end
-
-if CLIENT then
-	function SWEP:PrimaryAttack()
-
-	end
-
-	local colorGreen = Color(36, 160, 30)
-
-	hook.Add("TTTRenderEntityInfo", "ttt2_necro_defibrillator_display_info", function(tData)
-		local ent = tData:GetEntity()
-		local client = LocalPlayer()
-		local activeWeapon = client:GetActiveWeapon()
-
-		-- has to be a ragdoll
-		if ent:GetClass() ~= "prop_ragdoll" or not CORPSE.IsValidBody(ent) then return end
-
-		-- player has to hold a defibrillator
-		if not IsValid(activeWeapon) or activeWeapon:GetClass() ~= "weapon_ttth_necrodefi" then return end
-
-		-- ent has to be in usable range
-		if tData:GetEntityDistance() > 100 then return end
-
-		if activeWeapon:GetState() == DEFI_CHARGE then
-			tData:AddDescriptionLine(
-				LANG.TryTranslation("necrodefi_charging"),
-				COLOR_ORANGE
-			)
-
-			tData:SetOutlineColor(COLOR_ORANGE)
-
-			return
-		end
-
-		local ply = CORPSE.GetPlayer(ent)
-
-		if activeWeapon:GetState() ~= DEFI_BUSY and IsValid(ply) and ply:IsReviving() then
-			tData:AddDescriptionLine(
-				LANG.TryTranslation("necrodefi_player_already_reviving"),
-				COLOR_ORANGE
-			)
-
-			tData:SetOutlineColor(COLOR_ORANGE)
-
-			return
-		end
-
-		tData:AddDescriptionLine(
-			LANG.GetParamTranslation("necrodefi_hold_key_to_revive", {key = Key("+attack", "LEFT MOUSE")}),
-			colorGreen
-		)
-
-		if activeWeapon:GetState() ~= DEFI_BUSY then return end
-
-		local progress = math.min((CurTime() - activeWeapon:GetStartTime()) / activeWeapon:GetReviveTime(), 1.0)
-		local timeLeft = activeWeapon:GetReviveTime() - (CurTime() - activeWeapon:GetStartTime())
-
-		local x = 0.5 * ScrW()
-		local y = 0.5 * ScrH()
-		local w, h = 0.2 * ScrW(), 0.025 * ScrH()
-
-		y = 0.95 * y
-
-		surface.SetDrawColor(50, 50, 50, 220)
-		surface.DrawRect(x - 0.5 * w, y - h, w, h)
-		surface.SetDrawColor(clr(colorGreen))
-		surface.DrawOutlinedRect(x - 0.5 * w, y - h, w, h)
-		surface.SetDrawColor(clr(ColorAlpha(colorGreen, (0.5 + 0.15 * math.sin(CurTime() * 4)) * 255)))
-		surface.DrawRect(x - 0.5 * w + 2, y - h + 2, w * progress - 4, h - 4)
-
-		tData:AddDescriptionLine(
-			LANG.GetParamTranslation("necrodefi_revive_progress", {time = math.Round(timeLeft, 1)}),
-			colorGreen
-		)
-
-		tData:SetOutlineColor(colorGreen)
-	end)
+    function SWEP:OnRevive(ply, owner)
+        AddZombie(ply, owner)
+    end
 end
--- lua/terrortown/entities/roles/necromancer/shared.lua
+++ lua/terrortown/entities/roles/necromancer/shared.lua
@@ -1,154 +1,120 @@
 if SERVER then
-	AddCSLuaFile()
-end
-
--- CREATE CONVARS
-local defi_ammo = CreateConVar("ttt2_necrodefi_ammo", 0.34, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-CreateConVar("ttt2_necrodefi_revive_time", 3.0, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-CreateConVar("ttt2_necrodefi_error_time", 1.5, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-
--- SYNC CONVARS <> GLOBAL VARS
-if SERVER then
-	hook.Add("TTT2SyncGlobals", "TTT2NecroSyncGlobals", function()
-		SetGlobalFloat(defi_ammo:GetName(), defi_ammo:GetFloat())
-	end)
-
-	cvars.AddChangeCallback(defi_ammo:GetName(), function(name, old, new)
-		SetGlobalFloat(defi_ammo:GetName(), tonumber(new) == 1)
-	end, defi_ammo:GetName())
+    AddCSLuaFile()
 end
 
 roles.InitCustomTeam(ROLE.name, {
-	icon = "vgui/ttt/dynamic/roles/icon_necro",
-	color = Color(131, 55, 85, 255)
+    icon = "vgui/ttt/dynamic/roles/icon_necro",
+    color = Color(131, 55, 85, 255),
 })
 
 function ROLE:PreInitialize()
-	self.color = Color(131, 55, 85, 255)
+    self.color = Color(131, 55, 85, 255)
 
-	self.abbr = "necro"
-	self.defaultEquipment = SPECIAL_EQUIPMENT
-	self.score.surviveBonusMultiplier = 0.2
-	self.score.timelimitMultiplier = -0.5
-	self.score.killsMultiplier = 2
-	self.score.teamKillsMultiplier = -4
-	self.score.bodyFoundMuliplier = 0
+    self.abbr = "necro"
+    self.defaultEquipment = SPECIAL_EQUIPMENT
+    self.score.surviveBonusMultiplier = 0.2
+    self.score.timelimitMultiplier = -0.5
+    self.score.killsMultiplier = 2
+    self.score.teamKillsMultiplier = -4
+    self.score.bodyFoundMuliplier = 0
 
-	self.defaultTeam = TEAM_NECROMANCER
-	self.defaultEquipment = SPECIAL_EQUIPMENT
+    self.defaultTeam = TEAM_NECROMANCER
+    self.defaultEquipment = SPECIAL_EQUIPMENT
 
-	self.isOmniscientRole = true
+    self.isOmniscientRole = true
 
-	self.conVarData = {
-		pct = 0.15,
-		maximum = 1,
-		minPlayers = 7,
-		random = 40,
-		togglable = true,
-		credits = 2,
-		creditsAwardDeadEnable = 1,
-		creditsAwardKillEnable = 1,
-		shopFallback = SHOP_FALLBACK_TRAITOR
-	}
+    self.conVarData = {
+        pct = 0.15,
+        maximum = 1,
+        minPlayers = 7,
+        random = 40,
+        togglable = true,
+        credits = 2,
+        creditsAwardDeadEnable = 1,
+        creditsAwardKillEnable = 1,
+        shopFallback = SHOP_FALLBACK_TRAITOR,
+    }
 end
 
 if SERVER then
-	hook.Add("TTTOnCorpseCreated", "NecroAddedDeadBody", function(rag, ply)
-		if not IsValid(rag) or not IsValid(ply) then return end
+    hook.Add("TTTOnCorpseCreated", "NecroAddedDeadBody", function(rag, ply)
+        if not IsValid(rag) or not IsValid(ply) then
+            return
+        end
 
-		local mvObject = rag:AddMarkerVision("corpse_necro")
-		mvObject:SetOwner(ROLE_NECROMANCER)
-		mvObject:SetVisibleFor(VISIBLE_FOR_ROLE)
-		mvObject:SyncToClients()
-	end)
+        local mvObject = rag:AddMarkerVision("corpse_necro")
+        mvObject:SetOwner(ROLE_NECROMANCER)
+        mvObject:SetVisibleFor(VISIBLE_FOR_ROLE)
+        mvObject:SyncToClients()
+    end)
 
-	hook.Add("EntityRemoved", "NecroRemovedDeadBody", function(ent)
-		if not IsValid(ent) or ent:GetClass() ~= "prop_ragdoll" then return end
+    hook.Add("EntityRemoved", "NecroRemovedDeadBody", function(ent)
+        if not IsValid(ent) or ent:GetClass() ~= "prop_ragdoll" then
+            return
+        end
 
-		ent:RemoveMarkerVision("corpse_necro")
-	end)
+        ent:RemoveMarkerVision("corpse_necro")
+    end)
 
-	-- modify roles table of rolesetup addon
-	hook.Add("TTTAModifyRolesTable", "ModifyRoleNecroToInno", function(rolesTable)
-		local necromancers = rolesTable[ROLE_NECROMANCER]
+    -- modify roles table of rolesetup addon
+    hook.Add("TTTAModifyRolesTable", "ModifyRoleNecroToInno", function(rolesTable)
+        local necromancers = rolesTable[ROLE_NECROMANCER]
 
-		if not necromancers then return end
+        if not necromancers then
+            return
+        end
 
-		rolesTable[ROLE_INNOCENT] = rolesTable[ROLE_INNOCENT] + necromancers
-		rolesTable[ROLE_NECROMANCER] = 0
-	end)
+        rolesTable[ROLE_INNOCENT] = rolesTable[ROLE_INNOCENT] + necromancers
+        rolesTable[ROLE_NECROMANCER] = 0
+    end)
 
-	-- Give Loadout on respawn and rolechange
-	function ROLE:GiveRoleLoadout(ply, isRoleChange)
-		ply:GiveEquipmentWeapon("weapon_ttth_necrodefi")
-	end
+    -- Give Loadout on respawn and rolechange
+    function ROLE:GiveRoleLoadout(ply, isRoleChange)
+        ply:GiveEquipmentWeapon("weapon_ttth_necrodefi")
+    end
 
-	-- Remove Loadout on death and rolechange
-	function ROLE:RemoveRoleLoadout(ply, isRoleChange)
-		ply:StripWeapon("weapon_ttth_necrodefi")
-	end
+    -- Remove Loadout on death and rolechange
+    function ROLE:RemoveRoleLoadout(ply, isRoleChange)
+        ply:StripWeapon("weapon_ttth_necrodefi")
+    end
 
-	-- make sure that jackal and necro can not spawn together
-	hook.Add("TTT2ModifySelectableRoles", "TTTHJackOrNecro", function(selectableRoles)
-		if not selectableRoles[NECROMANCER] or not selectableRoles[JACKAL] then return end
+    -- make sure that jackal and necro can not spawn together
+    hook.Add("TTT2ModifySelectableRoles", "TTTHJackOrNecro", function(selectableRoles)
+        if not selectableRoles[NECROMANCER] or not selectableRoles[JACKAL] then
+            return
+        end
 
-		if math.random(2) == 2 then
-			selectableRoles[NECROMANCER] = nil
-		else
-			selectableRoles[JACKAL] = nil
-		end
-	end)
+        if math.random(2) == 2 then
+            selectableRoles[NECROMANCER] = nil
+        else
+            selectableRoles[JACKAL] = nil
+        end
+    end)
 end
 
 if CLIENT then
-	function ROLE:AddToSettingsMenu(parent)
-		local form = vgui.CreateTTT2Form(parent, "header_roles_additional")
+    local TryT = LANG.TryTranslation
+    local ParT = LANG.GetParamTranslation
 
-		form:MakeSlider({
-			serverConvar = "ttt2_necrodefi_ammo",
-			label = "label_necrodefi_ammo",
-			min = 0,
-			max = 5,
-			decimal = 2
-		})
+    local materialCorpse = Material("vgui/ttt/tid/tid_big_corpse")
 
-		form:MakeSlider({
-			serverConvar = "ttt2_necrodefi_revive_time",
-			label = "label_necrodefi_revive_time",
-			min = 0,
-			max = 30,
-			decimal = 1
-		})
+    hook.Add("TTT2RenderMarkerVisionInfo", "HUDDrawMarkerVisionNecroCorpse", function(mvData)
+        local client = LocalPlayer()
+        local ent = mvData:GetEntity()
+        local mvObject = mvData:GetMarkerVisionObject()
 
-		form:MakeSlider({
-			serverConvar = "ttt2_necrodefi_error_time",
-			label = "label_necrodefi_error_time",
-			min = 0,
-			max = 30,
-			decimal = 1
-		})
-	end
+        if not client:IsTerror() or not mvObject:IsObjectFor(ent, "corpse_necro") then
+            return
+        end
 
-	local TryT = LANG.TryTranslation
-	local ParT = LANG.GetParamTranslation
+        local distance = math.Round(util.HammerUnitsToMeters(mvData:GetEntityDistance()), 1)
 
-	local materialCorpse = Material("vgui/ttt/tid/tid_big_corpse")
+        mvData:EnableText()
 
-	hook.Add("TTT2RenderMarkerVisionInfo", "HUDDrawMarkerVisionNecroCorpse", function(mvData)
-		local client = LocalPlayer()
-		local ent = mvData:GetEntity()
-		local mvObject = mvData:GetMarkerVisionObject()
+        mvData:AddIcon(materialCorpse)
+        mvData:SetTitle(ParT("necro_corpse_player", { nick = CORPSE.GetPlayerNick(ent, "---") }))
 
-		if not client:IsTerror() or not mvObject:IsObjectFor(ent, "corpse_necro") then return end
-
-		local distance = math.Round(util.HammerUnitsToMeters(mvData:GetEntityDistance()), 1)
-
-		mvData:EnableText()
-
-		mvData:AddIcon(materialCorpse)
-		mvData:SetTitle(ParT("necro_corpse_player", {nick = CORPSE.GetPlayerNick(ent, "---")}))
-
-		mvData:AddDescriptionLine(ParT("marker_vision_distance", {distance = distance}))
-		mvData:AddDescriptionLine(TryT(mvObject:GetVisibleForTranslationKey()), COLOR_SLATEGRAY)
-	end)
+        mvData:AddDescriptionLine(ParT("marker_vision_distance", { distance = distance }))
+        mvData:AddDescriptionLine(TryT(mvObject:GetVisibleForTranslationKey()), COLOR_SLATEGRAY)
+    end)
 end
